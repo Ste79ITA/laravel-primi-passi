e2d51da1b300a916d8b35a9a1067012eed81ea56
Ste79ITA <nastri.s@gmail.com>
2023-12-07 17:01:27 +0100

    homepage and data

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+
+    $title = 'Welcome To Laravel - First Steps';
+
+
+    return view('home', compact("title"));
 });
